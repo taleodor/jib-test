8afebac6e30b10a1f06be1777949157f3c83d1d4
Pavel Shukhman <taleodor@gmail.com>
2020-08-29 14:45:33 -0400

    initial commit

---
+++ src/test/java/io/reliza/jibtest/JibTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reliza.jibtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
